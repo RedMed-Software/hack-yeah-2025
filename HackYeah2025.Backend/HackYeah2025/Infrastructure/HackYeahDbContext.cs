fb983f4b07d39e80756aa09308b5d56048ec911b
Kamil Złotek <156350@stud.prz.edu.pl>
2025-10-04 12:05:07 +0200

    Hello World

---
+++ HackYeah2025.Backend/HackYeah2025/Infrastructure/HackYeahDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HackYeah2025.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackYeah2025.Infrastructure;
+
+public sealed class HackYeahDbContext : DbContext
+{
+    public DbSet<Foo> Foos { get; set; }
+
+    public HackYeahDbContext(DbContextOptions<HackYeahDbContext> options) : base(options) 
+    { 
+    
+    }
+}
---
+++ HackYeah2025.Backend/HackYeah2025/Program.cs
@@ -0,0 +1,25 @@
+using HackYeah2025.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<HackYeahDbContext>(o =>
+    o.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
+);
+
+WebApplication app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
