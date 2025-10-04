72e5428e1cc8f6cf493e6a576d28b62f174b6415
Atholn <mz3pr0gr4mmm3r@gmail.com>
2025-10-04 13:08:17 +0200

    add init models

---
+++ HackYeah2025.Backend/HackYeah2025/Infrastructure/Enums/UserType.cs
@@ -0,0 +1,11 @@
+﻿namespace HackYeah2025.Infrastructure.Enums
+{
+    public enum UserType
+    {
+        None = 0,
+        Volunteer = 1,
+        VolunteerCoordinator = 2,
+        Organization = 3,
+        Admin = 4,
+    }
+}
---
+++ HackYeah2025.Backend/HackYeah2025/Infrastructure/Models/VolunteerEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace HackYeah2025.Infrastructure.Models;
+
+public class VolunteerEvent
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Localization { get; set; }
+    public DateTimeOffset DateFrom { get; set; } 
+    public DateTimeOffset DateTo { get; set; }
+
+    public Guid OrganizationId { get; set; }
+    public List<Requirement> Requirements { get; set; }
+}
