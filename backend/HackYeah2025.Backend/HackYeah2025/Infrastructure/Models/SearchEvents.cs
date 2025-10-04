070c21f88ed9821a302cd8bd91e058e171f8d807
Atholn <mz3pr0gr4mmm3r@gmail.com>
2025-10-04 22:27:41 +0200

    UPGRADE SEARCH

--- backend/HackYeah2025.Backend/HackYeah2025/Infrastructure/Models/SearchEvents.cs
+++ backend/HackYeah2025.Backend/HackYeah2025/Infrastructure/Models/SearchEvents.cs
@@ -1,8 +1,11 @@
-﻿namespace HackYeah2025.Infrastructure.Models
+﻿using HackYeah2025.Infrastructure.Enums;
+
+namespace HackYeah2025.Infrastructure.Models
 {
     public class SearchEvents
     {
         public Guid? OrganizerId { get; set; }
+        public EventStatus? EventStatus { get; set; }
         public string? Query { get; set; } = null;
     }
 }
