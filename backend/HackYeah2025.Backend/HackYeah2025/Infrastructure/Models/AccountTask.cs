6e972fa6de26e98a6544d0598bb06438a6a2ac29
Kamil Złotek <156350@stud.prz.edu.pl>
2025-10-04 21:02:21 +0200

    Tasks

---
+++ backend/HackYeah2025.Backend/HackYeah2025/Infrastructure/Models/AccountTask.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HackYeah2025.Infrastructure.Models;
+
+public sealed class AccountTask
+{
+    public required Guid AccountId { get; set; }
+    public required Guid TaskItemId { get; set; }
+
+    public Account? Account { get; set; }
+    public TaskItem? Task { get; set; }
+}
+
+public sealed class DbAccountTaskEntityTypeConfiguration : IEntityTypeConfiguration<AccountTask>
+{
+    public void Configure(EntityTypeBuilder<AccountTask> builder)
+    {
+        builder.HasKey(ut => new { ut.AccountId, ut.TaskItemId });
+
+        builder.HasOne(ut => ut.Account)
+            .WithMany(a => a.AccountTasks)
+            .HasForeignKey(ut => ut.AccountId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(ut => ut.Task)
+            .WithMany(t => t.AccountTasks)
+            .HasForeignKey(ut => ut.TaskItemId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ backend/HackYeah2025.Backend/HackYeah2025/Migrations/20251004190058_Tasks.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HackYeah2025.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+
+            migrationBuilder.CreateTable(
+                name: "TaskItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    DateStart = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DateEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskItems_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountTasks",
+                columns: table => new
+                {
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaskItemId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTasks", x => new { x.AccountId, x.TaskItemId });
+                    table.ForeignKey(
+                        name: "FK_AccountTasks_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountTasks_TaskItems_TaskItemId",
+                        column: x => x.TaskItemId,
+                        principalTable: "TaskItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Volunteers",
+                keyColumn: "Id",
+                keyValue: new Guid("9f064bb8-162d-4e49-88f5-2e5f5f9a7ab8"),
+                columns: new[] { "Availability", "Languages", "Skills" },
+                values: new object[] { new Dictionary<string, string> { ["tuesday_thursday"] = "Wtorki i czwartki 16:00 – 20:00", ["weekends"] = "Weekendy według ustaleń" }, new Dictionary<string, string> { ["Polski"] = "C2", ["Angielski"] = "C1", ["Ukraiński"] = "B1" }, new Dictionary<string, string> { ["Komunikacja i moderacja"] = "Zaawansowany", ["Animacja czasu wolnego"] = "Średniozaawansowany", ["Pierwsza pomoc"] = "Podstawowy", ["Planowanie wydarzeń"] = "Zaawansowany" } });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountTasks_TaskItemId",
+                table: "AccountTasks",
+                column: "TaskItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItems_EventId",
+                table: "TaskItems",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountTasks");
+
+            migrationBuilder.DropTable(
+                name: "TaskItems");
+
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Task_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Volunteers",
+                keyColumn: "Id",
+                keyValue: new Guid("9f064bb8-162d-4e49-88f5-2e5f5f9a7ab8"),
+                columns: new[] { "Availability", "Languages", "Skills" },
+                values: new object[] { new Dictionary<string, string> { ["tuesday_thursday"] = "Wtorki i czwartki 16:00 – 20:00", ["weekends"] = "Weekendy według ustaleń" }, new Dictionary<string, string> { ["Polski"] = "C2", ["Angielski"] = "C1", ["Ukraiński"] = "B1" }, new Dictionary<string, string> { ["Komunikacja i moderacja"] = "Zaawansowany", ["Animacja czasu wolnego"] = "Średniozaawansowany", ["Pierwsza pomoc"] = "Podstawowy", ["Planowanie wydarzeń"] = "Zaawansowany" } });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_EventId",
+                table: "Task",
+                column: "EventId");
+        }
+    }
+}
