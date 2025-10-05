1e3a9a74c8024e590bbac99bc552209ef8ac52d5
Kamil Złotek <156350@stud.prz.edu.pl>
2025-10-05 10:00:51 +0200

    account id

--- backend/HackYeah2025.Backend/HackYeah2025/Features/OrganizerController.cs
+++ backend/HackYeah2025.Backend/HackYeah2025/Features/OrganizerController.cs
@@ -1,23 +1,21 @@
+using HackYeah2025.Infrastructure;
 using HackYeah2025.Infrastructure.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HackYeah2025.Features;
 
 [Route("api/[controller]")]
 [ApiController]
-public class OrganizerController : ControllerBase
+public class OrganizerController(
+    HackYeahDbContext dbContext,
+    IOrganizerService organizerService
+) : ControllerBase
 {
-    private readonly IOrganizerService _organizerService;
-
-    public OrganizerController(IOrganizerService organizerService)
-    {
-        _organizerService = organizerService;
-    }
-
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<Organizer>> Get(Guid id, CancellationToken cancellationToken)
     {
-        Organizer? organizer = await _organizerService.GetByIdAsync(id, cancellationToken);
+        Organizer? organizer = await organizerService.GetByIdAsync(id, cancellationToken);
 
         if (organizer is null)
         {
@@ -25,5 +23,19 @@ public class OrganizerController : ControllerBase
         }
 
         return Ok(organizer);
+    }
+
+    [HttpGet("{id:guid}/get-account-id")]
+    public async Task<ActionResult<Guid>> GetAccountIdByOrganizerId(Guid id, CancellationToken cancellationToken)
+    {
+        Organizer? organizer = await dbContext.Organizers
+            .Where(o => o.Id == id)
+            .Include(o => o.Account)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (organizer is null)
+            return NotFound("Organizer or related account not found.");
+
+        return Ok(organizer.Account.Id);
     }
 }
